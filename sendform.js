cb2c384bec0bd81089816c2dae304e4557718659
Ian Clark <ianclark226@gmail.com>
2020-01-23 18:32:15 -0500

    1/23

--- sendform.js
+++ sendform.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var emailFrom = $("#email").val();
     var emailBody = $("#message").val();
     emailBody = emailBody + "%0A" + "%0A" + "Sender's Name: " + senderName + " | " + "  Sender's Email: " + emailFrom + "%0A";
-    window.location.href = "mailto:ianclark226@gmail.com?&subject=Billboard%20inquiry&body=" + emailBody;
+    window.location.href = "mailto:ianclark226@gmail.com" + emailBody;
     console.log("itsent");
     });    
   });
