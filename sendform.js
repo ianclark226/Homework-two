493f5ada3e5616e95ee64c27204b826690ebde83
Ian Clark <ianclark226@gmail.com>
2020-03-21 19:09:25 -0400

    update

--- sendform.js
+++ sendform.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var emailFrom = $("#email").val();
     var emailBody = $("#message").val();
     emailBody = emailBody + "%0A" + "%0A" + "Sender's Name: " + senderName + " | " + "  Sender's Email: " + emailFrom + "%0A";
-    window.location.href = "mailto:ianclark226@gmail.com" + emailBody;
+    window.location.href = "mailto:ianclark226@gmail.com?&body=" + emailBody;
     console.log("itsent");
     });    
   });
